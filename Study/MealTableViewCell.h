9f6a81a903ca393a40cd8ab28aed7b162650dff2
lvzheng <lvzheng@chexiang.com>
2016-08-03 13:33:14 +0800

    + Add Meal table view cell

---
+++ Study/MealTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MealTableViewCell.h
+//  Study
+//
+//  Created by LvZheng on 16/8/3.
+//  Copyright © 2016年 LvZheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MealTableViewCell : UITableViewCell
+
+@end
---
+++ Study/MealTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.m
+//  Study
+//
+//  Created by LvZheng on 16/8/3.
+//  Copyright © 2016年 LvZheng. All rights reserved.
+//
+
+#import "MealTableViewCell.h"
+
+@implementation MealTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
